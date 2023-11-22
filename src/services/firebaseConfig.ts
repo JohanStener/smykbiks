4c08e5f34b47979bcc92d2e986dfdfc025f71932
JohanStener <113592529+JohanStener@users.noreply.github.com>
2023-11-22 21:10:44 +0100

    Update firebaseConfig.ts

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -1,13 +1,1 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCM1HLkjw_WOX5oyS8IrbHIUtGGnQAfDrg",
-  authDomain: "smykkebiksen.firebaseapp.com",
-  projectId: "smykkebiksen",
-  storageBucket: "smykkebiksen.appspot.com",
-  messagingSenderId: "108540561555",
-  appId: "1:108540561555:web:6534d87d6fee4ea324a566",
-  measurementId: "G-TKBS1TRBPP",
-};
-
-export { firebaseConfig };
